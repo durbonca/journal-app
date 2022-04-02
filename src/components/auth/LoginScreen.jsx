0e63f4aaca02212557fc1f6d012eab2e43cf690c
durbonca <durbonca@gmail.com>
2022-04-02 14:20:08 -0300

    creando login screen

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 
 export const LoginScreen = () => {
   return (
-    <div>LoginScreen</div>
+    <>
+      <h3>
+        Login
+      </h3>
+      <form>
+        <input 
+          type="text" 
+          placeholder="email"
+          name='email'
+        />
+
+        <input 
+          type="password" 
+          placeholder="password"
+          name='password'
+        />
+
+        <button
+          type="submit"
+        >
+          Login
+        </button>
+        <hr />
+        Google
+      </form>
+    </>
   )
 }
