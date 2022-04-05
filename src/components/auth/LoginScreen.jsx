ec1c28294b4c09d01b7499c60ac98d4e2f710577
durbonca <durbonca@gmail.com>
2022-04-05 09:52:32 -0400

    config redux middleware for async login task

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,7 +1,7 @@
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { useForm } from '../../hooks/useForms';
-import { login } from '../../actions/auth';
+import { login, startLoginEmailPassword } from '../../actions/auth';
 
 export const LoginScreen = () => {
 
@@ -15,7 +15,7 @@ export const LoginScreen = () => {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    dispatch( login(123456, 'Manolo') )
+    dispatch( startLoginEmailPassword(email, password) )
   }
 
   return (
